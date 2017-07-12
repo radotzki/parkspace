9d1e8fdc1defd05991896ad06bf367364264cfb2
Itay Radotzki <radotzki@gmail.com>
2017-07-12 16:35:51 +0300

    remove junk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
-    rootPage: any = TabsPage;
+    rootPage: any;
 
     constructor(
         private platform: Platform,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,21 +12,12 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HttpModule } from '@angular/http';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
     declarations: [
         MyApp,
-        AboutPage,
-        ContactPage,
-        HomePage,
-        TabsPage,
         TimePicker,
         Meme,
     ],
@@ -42,10 +33,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     bootstrap: [IonicApp],
     entryComponents: [
         MyApp,
-        AboutPage,
-        ContactPage,
-        HomePage,
-        TabsPage,
         TimePicker,
         Meme,
     ],
